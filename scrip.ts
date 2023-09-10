3d549b61a71b7f91177b58766685adc478c7cbea
polo <polo@contoso.com>
2023-09-10 07:40:49 -0600

    addd

--- scrip.ts
+++ scrip.ts
@@ -1,5 +1,6 @@
 import express,{Request,Response} from "express"
-
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
 const port = process.env.PORT || 3000;
 
 const app=express()
@@ -7,7 +8,33 @@ const app=express()
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 
+app.get("/users",async(req:Request,res:Response)=>{
+  try{
+    const users=await prisma.sample.findMany();
+    return res.json(users)
+  }catch(err){
+    return res.status(500).json({msg:"Server Error",err})
+  }
+})
 
+app.post("/users",async(req:Request,res:Response)=>{
+  try{
+    const {name,email,age}=req.body
+    if(!name || !email || !age)return res.status(403).json({msg:"All camps are required"})
+    
+    const  newUser= await prisma.sample.create({
+      data:{
+        name,
+        email,
+        age
+      }
+    })
+    return res.status(201).json(newUser)
+   
+  }catch(err){
+    return res.status(500).json({msg:"Server Error",err})
+  }
+})
 app.get("/",(req:Request,res:Response)=>{
     return res.send("Hello Worl")
 })
